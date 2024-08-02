80a225327b6c5f7c45754e07c4f02b18230b1bcf
Lorena Gomes <77419621+lorena-gc@users.noreply.github.com>
2024-08-02 18:52:10 +0100

    Updated Records

--- src/main/java/com/vfl/mutirao_solidario/controller/dto/EventRequest.java
+++ src/main/java/com/vfl/mutirao_solidario/controller/dto/EventRequest.java
@@ -1,4 +1,10 @@
 package com.vfl.mutirao_solidario.controller.dto;
 
-public record EventRequest(Long organizerId, String title, String description, String location, Integer minVolunteers, Integer maxVolunteers) {
+public record EventRequest(
+        Long organizerId,
+        String title,
+        String description,
+        String location,
+        Integer minVolunteers,
+        Integer maxVolunteers) {
 }
--- src/main/java/com/vfl/mutirao_solidario/controller/dto/EventUpdate.java
+++ src/main/java/com/vfl/mutirao_solidario/controller/dto/EventUpdate.java
@@ -2,5 +2,13 @@ package com.vfl.mutirao_solidario.controller.dto;
 
 import com.vfl.mutirao_solidario.enums.Status;
 
-public record EventUpdate(Long id, Long organizerId, String title, String description, String location, String minVolunteers, String maxVolunteers, Status status) {
+public record EventUpdate(
+        Long id,
+        Long organizerId,
+        String title,
+        String description,
+        String location,
+        Integer minVolunteers,
+        Integer maxVolunteers,
+        Status status) {
 }
