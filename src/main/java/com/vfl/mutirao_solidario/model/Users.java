4560d8657e3e563104e81ed57f6b6b88c7c6e59f
Lorena Gomes <77419621+lorena-gc@users.noreply.github.com>
2024-08-02 16:40:30 +0100

    Created User model

---
+++ src/main/java/com/vfl/mutirao_solidario/model/Users.java
@@ -0,0 +1,15 @@
+package com.vfl.mutirao_solidario.model;
+
+public class Users {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+    
+}
