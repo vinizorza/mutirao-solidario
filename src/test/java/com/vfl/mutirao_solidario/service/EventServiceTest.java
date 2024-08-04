c294842798962ea22a62b5c0577fbe62a5cabf9c
Lorena Gomes <77419621+lorena-gc@users.noreply.github.com>
2024-08-04 16:07:41 +0100

    Created EventServiceTest and RegistrationServiceTest

---
+++ src/test/java/com/vfl/mutirao_solidario/service/EventServiceTest.java
@@ -0,0 +1,151 @@
+package com.vfl.mutirao_solidario.service;
+
+import com.vfl.mutirao_solidario.controller.dto.EventRequest;
+import com.vfl.mutirao_solidario.controller.dto.EventResponse;
+import com.vfl.mutirao_solidario.controller.dto.EventUpdate;
+import com.vfl.mutirao_solidario.enums.Status;
+import com.vfl.mutirao_solidario.model.Event;
+import com.vfl.mutirao_solidario.model.User;
+import com.vfl.mutirao_solidario.repository.EventRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class EventServiceTest {
+
+    @Mock
+    private EventRepository eventRepository;
+
+    @Mock
+    private AuthenticationService authenticationService;
+
+    @InjectMocks
+    private EventService eventService;
+
+    @Captor
+    private ArgumentCaptor<Event> eventCaptor;
+
+    private static EventRequest getEventRequest(Long organizerId) {
+        return new EventRequest(
+                organizerId,
+                "Limpeza margem do rio pinheiros",
+                "Retirar o lixo jogado na margem do rio",
+                1236.0,
+                12235.2,
+                10,
+                50,
+                LocalDateTime.now()
+        );
+    }
+
+    private static Event getEvent(Long eventId, Long organizerId, Status status, LocalDateTime date, Double latitude, Double longitude){
+        return Event.builder()
+                .id(eventId)
+                .organizer(User.builder().id(organizerId).build())
+                .status(status)
+                .date(date)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build();
+    }
+
+    @Test
+    void createEventWithSucess() {
+
+        EventRequest eventRequest = getEventRequest(3L);
+
+        doNothing().when(authenticationService).validateUser(eventRequest.organizerId());
+
+        eventService.create(eventRequest);
+
+        verify(authenticationService, times(1)).validateUser(eventRequest.organizerId());
+        verify(eventRepository,times(1)).save(eventCaptor.capture());
+
+        Event savedEvent = eventCaptor.getValue();
+        assertThat(savedEvent.getOrganizer().getId()).isEqualTo(3L);
+        assertThat(savedEvent.getStatus()).isEqualTo(Status.ORGANIZING);
+        assertThat(assertThat(savedEvent.getDescription())).isNotNull();
+    }
+
+    @Test
+    public void createEventThrowsExceptionWhenUserNotHavePermission() {
+
+        EventRequest eventRequest = getEventRequest(2L);
+
+        doThrow(new RuntimeException("User does not have the permission")).when(authenticationService).validateUser(eventRequest.organizerId());
+
+        assertThatThrownBy(() -> eventService.create(eventRequest))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("User does not have the permission");
+
+        verify(authenticationService, times(1)).validateUser(eventRequest.organizerId());
+        verifyNoInteractions(eventRepository);
+    }
+
+    @Test
+    void getEventsByFilter() {
+
+        List<Event> eventList = Arrays.asList(
+                getEvent(1L, 1L, Status.CONFIRMED, LocalDateTime.of(2024,8,20,15,30), -19.918780765956157, -43.94689818810624),
+                getEvent(2L, 1L, Status.CONFIRMED, LocalDateTime.of(2024,8,10,15,30), -18.78900340181768, -44.42081879334378),
+                getEvent(3L, 2L, Status.ORGANIZING, LocalDateTime.of(2024,8,15,15,30), -2.509562271953361, -44.235443044838455));
+
+        when(eventRepository.findEvents(null, null, null, Arrays.asList(Status.CONFIRMED, Status.ORGANIZING))).thenReturn(eventList);
+
+        List<EventResponse> responses = eventService.getEventsByFilter(-19.943576, -43.967696, 150L, null, null,null,Arrays.asList(Status.CONFIRMED, Status.ORGANIZING));
+
+        assertThat(responses).hasSize(2);
+
+        verify(eventRepository,times(1)).findEvents(null, null, null, Arrays.asList(Status.CONFIRMED, Status.ORGANIZING));
+    }
+
+
+    @Test
+    void deleteEvent() {
+        Long eventId = 2L;
+
+        eventService.delete(eventId);
+
+        verify(eventRepository, times(1)).deleteById(eventId);
+    }
+
+    @Test
+    void updateSucessfully() {
+        EventUpdate eventUpdate = new EventUpdate(
+                1L,
+                "Limpeza margem do rio pinheiros",
+                "Retirar o lixo jogado na margem do rio",
+                10,
+                60,
+                Status.CONFIRMED,
+                LocalDateTime.now(),
+                12235.2,
+                1000.2
+        );
+
+        doNothing().when(authenticationService).validateUser(eventUpdate.organizerId());
+
+        eventService.update(eventUpdate, 1L);
+
+        verify(authenticationService, times(1)).validateUser(eventUpdate.organizerId());
+        verify(eventRepository,times(1)).save(eventCaptor.capture());
+
+        Event savedEvent = eventCaptor.getValue();
+        assertThat(savedEvent.getId()).isEqualTo(1L);
+        assertThat(savedEvent.getStatus()).isEqualTo(Status.CONFIRMED);
+        assertThat(assertThat(savedEvent.getDescription())).isNotNull();
+    }
+}
---
+++ src/test/java/com/vfl/mutirao_solidario/service/RegistrationServiceTest.java
@@ -0,0 +1,141 @@
+package com.vfl.mutirao_solidario.service;
+
+import com.vfl.mutirao_solidario.controller.dto.RegistrationRequest;
+import com.vfl.mutirao_solidario.controller.dto.RegistrationResponse;
+import com.vfl.mutirao_solidario.model.Event;
+import com.vfl.mutirao_solidario.model.Registration;
+import com.vfl.mutirao_solidario.model.User;
+import com.vfl.mutirao_solidario.repository.RegistrationRepository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RegistrationServiceTest {
+
+    @Mock
+    private RegistrationRepository registrationRepository;
+
+    @Mock
+    private AuthenticationService authenticationService;
+
+    @InjectMocks
+    private RegistrationService registrationService;
+
+    @Captor
+    private ArgumentCaptor<Registration> registrationCaptor;
+
+    private static User getUser(Long userId){
+        return User.builder()
+                .id(userId)
+                .build();
+    }
+
+    private static Event getEvent(Long eventId){
+        return Event.builder()
+                .id(eventId)
+                .build();
+    }
+
+    @Test
+    void joinEventWithSucess() {
+        Long userId = 1L;
+        Long eventId = 2L;
+        RegistrationRequest registrationRequest = new RegistrationRequest(eventId, userId);
+
+        doNothing().when(authenticationService).validateUser(userId);
+
+        registrationService.joinEvent(registrationRequest);
+
+        verify(authenticationService, times(1)).validateUser(userId);
+        verify(registrationRepository, times(1)).save(registrationCaptor.capture());
+
+        Registration savedRegistration = registrationCaptor.getValue();
+        assertThat(savedRegistration.getEvent().getId()).isEqualTo(eventId);
+        assertThat(savedRegistration.getUser().getId()).isEqualTo(userId);
+    }
+
+    @Test
+    public void joinEventThrowsExceptionWhenUserNotHavePermission() {
+        Long userId = 1L;
+        Long eventId = 2L;
+        RegistrationRequest registrationRequest = new RegistrationRequest(eventId, userId);
+
+        doThrow(new RuntimeException("User does not have the permission")).when(authenticationService).validateUser(userId);
+
+        assertThatThrownBy(() -> registrationService.joinEvent(registrationRequest))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("User does not have the permission");
+
+        verify(authenticationService, times(1)).validateUser(userId);
+        verifyNoInteractions(registrationRepository);
+    }
+
+    @Test
+    void deleteRegistration() {
+        Long registrationId = 1L;
+
+        registrationService.delete(registrationId);
+
+        verify(registrationRepository, times(1)).deleteById(registrationId);
+    }
+
+    @Test
+    void getRegistrationByUserId() {
+        User user1 = getUser(1L);
+        Event event1 = getEvent(1L);
+        Event event2 = getEvent(2L);
+
+        Registration registration1 = new Registration(1L,event1, user1);
+        Registration registration2 = new Registration(2L, event2, user1);
+
+        List<Registration> registrationList = Arrays.asList(registration1, registration2);
+
+        when(registrationRepository.findByUserId(user1.getId())).thenReturn(registrationList);
+
+        List<RegistrationResponse> responses = registrationService.getRegistrationByUserId(user1.getId());
+
+        assertThat(responses).hasSize(2);
+        assertThat(responses.get(0).userId()).isEqualTo(user1.getId());
+        assertThat(responses.get(0).eventId()).isEqualTo(1L);
+        assertThat(responses.get(1).eventId()).isEqualTo(2L);
+
+        verify(registrationRepository, times(1)).findByUserId(user1.getId());
+
+    }
+
+    @Test
+    void getRegistrationByEventId() {
+        User user1 = getUser(1L);
+        User user2 = getUser(2L);
+        Event event1 = getEvent(2L);
+
+        Registration registration1 = new Registration(1L,event1, user1);
+        Registration registration2 = new Registration(2L, event1, user2);
+
+        List<Registration> registrationList = Arrays.asList(registration1, registration2);
+
+        when(registrationRepository.findByEventId(event1.getId())).thenReturn(registrationList);
+
+        List<RegistrationResponse> responses = registrationService.getRegistrationByEventId(event1.getId());
+
+        assertThat(responses).hasSize(2);
+        assertThat(responses.get(0).eventId()).isEqualTo(event1.getId());
+        assertThat(responses.get(0).userId()).isEqualTo(1L);
+        assertThat(responses.get(1).userId()).isEqualTo(2L);
+
+        verify(registrationRepository, times(1)).findByEventId(event1.getId());
+    }
+}
